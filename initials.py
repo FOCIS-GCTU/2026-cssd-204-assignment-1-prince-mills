991509181b78275d9ea5b7a21f10c0241671fea1
prince-mills <2425401763@live.gctu.edu.gh>
2026-05-31 21:19:44 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+# Name: Prince Mills Akortsu
+# STUDENT ID: 2425401763
+# Email: 2425401763@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+# On my honor, Prince Mills Akortsu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
+    
+    print()
+    print("...PMA")
+    print()
+    print("...PPPPPPPPPP..........MMMM....MMMM............AAAA.........")
+    print("...PPPPPPPPPPP.........MMMM....MMMM...........AA..AA........")
+    print("...PP........PP........MM.MM..MM.MM..........AA....AA.......")
+    print("...PP........PP........MM.MM..MM.MM.........AA......AA......")
+    print("...PPPPPPPPPPP.........MM.MM..MM.MM........AA........AA.....")
+    print("...PPPPPPPPPP..........MM..MMMM..MM........AAAAAAAAAAAA.....")
+    print("...PP..................MM..MMMM..MM........AAAAAAAAAAAA.....")
+    print("...PP..................MM...MM...MM........AA........AA.....")
+    print("...PP.............**...MM...MM...MM...**...AA........AA...**")
+    print("...PP.............**...MM...MM...MM...**...AA........AA...**")
+    print()
 
 main()
